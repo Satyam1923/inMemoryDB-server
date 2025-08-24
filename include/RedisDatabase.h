720d1db0722325701de3804e0735d6ad65377f10
Satyam Rathor <satyamr1923@gamil.com>
2025-08-24 22:39:20 +0530

    list operations

--- include/RedisDatabase.h
+++ include/RedisDatabase.h
@@ -14,17 +14,27 @@ class RedisDatabase{
     bool flushAll();
 
     //key value operations
-    void set(const std::string&key,const std::string&value);
-    bool get(const std::string&key, std::string&value);
+    void set(const std::string &key,const std::string&value);
+    bool get(const std::string &key, std::string&value);
     std::vector<std::string> keys();
     std::string type(const std::string&key);
-    bool del(const std::string&key);
+    bool del(const std::string &key);
     bool expire(const std::string &key, int seconds);
     void purgeExpired();
-    bool rename(const std::string&oldkey,const std::string&newkey);
+    bool rename(const std::string &oldkey,const std::string&newkey);
 
+    //list operations
+    ssize_t llen(const std::string &key);
+    void lpush(const std::string &key, const std::string &value);
+    void rpush(const std::string &key, const std::string &value);
+    bool lpop(const std::string &key,std::string& value);
+    bool rpop(const std::string &key, std::string &value);
+    int lrem(const std::string &key,int count,const std::string &value);
+    bool lindex(const std::string &key,int index,std::string &value);
+    bool lset(const std::string &key, int index, const std::string &value);
 
-    bool dump(const std::string& filename);
+    // db operations
+    bool dump(const std::string &filename);
     bool load(const std::string& filename);
 
     private:
